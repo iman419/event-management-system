78a374e6a6c74ae46e507bc661344f5d340498e0
iman419 <iman.hadzihasanovic@stu.ibu.edu.ba>
2025-11-02 21:43:37 +0100

    Delete backend/dao directory

--- backend/dao/UserDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/BaseDao.php';
-
-class UserDao extends BaseDao {
-  public function __construct() {
-    parent::__construct('users');
-  }
-}
--- backend/dao/VenueDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/BaseDao.php';
-
-class VenueDao extends BaseDao {
-  public function __construct() {
-    parent::__construct('venues');
-  }
-}
